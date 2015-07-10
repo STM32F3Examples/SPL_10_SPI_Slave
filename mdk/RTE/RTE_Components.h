9832ea5fb82ad780ae726bf216363b359cfda3e5
Manuel Limones <limonesu.me@gmail.com>
2015-07-09 22:54:42 -0500

    rename mdk project

--- mdk/RTE/RTE_Components.h
+++ mdk/RTE/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'SPL_08_SPI_STM32F3' 
+ * Project: 'SPL_10_SPI_Slave_STM32F3' 
  * Target:  'Target 1' 
  */
 
